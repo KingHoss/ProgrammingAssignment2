4747055718fbe5e12b527457b5152c4035b972f8
KingHoss <31486784+KingHoss@users.noreply.github.com>
2017-09-03 22:20:28 -0400

    Added codes for functions
    By Zane Tavana

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,25 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCatcheMatrix <- function(x = numeric()) {
+  m <- matrix(NaN)                #initialize to NULL
+  set <- function(y) {
+    x <<- y
+    m <<- matrix(NaN)
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
-
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve()                 #calls getsolve above to return m
+  if(m[1,1] != 'NaN') {             #if the returned m from getsolve above has value, use it
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()                   #else return x from get above and store it in data
+  m <- solve(data)                  #find the inverse of data
+  x$setsolve(m)            #call to getsolve above to store and cache m for subsequent calls
+  m                        #return m
 }
